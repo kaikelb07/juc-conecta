04b7742de90d797989a1b325c2c029f07403e50a
Kaike <kaikelb07@gmail.com>
2026-04-29 21:28:38 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
         {/* Logo */}
         <div className="mb-8 animate-fade-in">
           <Image
-            src="/public/images/logo.png"
+            src="/images/logo.png"
             alt="Juc.Conecta - Marketing Católico"
             width={280}
             height={280}
